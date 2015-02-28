8a19f82048563d8e8bb34de2d5cc51546d1df27e
mailsonncosta <mailsonncosta@gmail.com>
2015-02-28 11:07:35 -0300

    continuação

--- core/src/com/restinhosoft/shakethisbottle/exception/SoundManagerNotStartedException.java
+++
@@ -1,9 +0,0 @@
-package com.restinhosoft.shakethisbottle.exception;
-
-public class SoundManagerNotStartedException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-	
-	public SoundManagerNotStartedException() { super("No message"); }
-		
-}
